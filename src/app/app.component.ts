c2f9ecc281dd1defc19bd2d71ad407cc232a24ec
鲍世龙 <1282936365@qq.com>
2022-05-25 22:21:48 +0800

    添加动态加载

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-import {CusBtnComponent} from "@dream_light_color/cus-btn";
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {mergeMap, of, zip} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(
+    private httpClient: HttpClient,
+  ) {
+  }
   switchValue = false
+  ngOnInit() {
+    of(new Date().getTime())
+      .pipe(
+        mergeMap(date => zip(
+          this.httpClient.get('/index.html?v='+date),
+          this.httpClient.get('/main.js?v='+date),
+          this.httpClient.get('/polyfills.js?v='+date),
+          this.httpClient.get('/runtime.js?v='+date),
+          this.httpClient.get('/style.css?v='+date)
+        ))
+      ).subscribe(_ => location.reload())
+  }
 }
